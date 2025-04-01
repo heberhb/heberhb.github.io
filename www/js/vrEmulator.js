9cadf8e43cf10645c7e3afc7a09b1dcad5c45ea5
Heber_Hb <heber20014@gmaiL.com>
2025-04-01 15:51:20 -0300

    Adicionando projeto VrEmulator

---
+++ www/js/vrEmulator.js
@@ -0,0 +1,29 @@
+document.getElementById('startCapture').addEventListener('click', async () => {
+    try {
+        // Solicita permissão para capturar a tela
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+            video: true, // Captura apenas o vídeo
+            audio: false // Sem áudio
+        });
+
+        // Obtém o elemento <video>
+        const videoElement = document.getElementById('screenVideo');
+        // Obtém o elemento <video>
+        const videoElement1 = document.getElementById('screenVideo1');
+
+        // Define o fluxo de mídia no <video>
+        videoElement.srcObject = mediaStream;
+        // Define o fluxo de mídia no <video>
+        videoElement1.srcObject = mediaStream;
+    } catch (err) {
+        console.error('Erro ao capturar a tela:', err);
+    }
+});
+
+document.getElementById('screenVideo').addEventListener('click', (event) => {
+    // Para encerrar a captura quando o stream for interrompido
+    mediaStream.getVideoTracks()[0].addEventListener('ended', () => {
+        alert('Captura de tela encerrada.');
+    })
+    console.log('Clique detectado no screenVideo');
+});
